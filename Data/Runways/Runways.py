953f58ab1c213817883bae1434710331a22de26a
Nor97 <53235829+Nor97@users.noreply.github.com>
2019-09-03 14:33:09 -0400

    Update Runways.py

--- Data/Runways/Runways.py
+++ Data/Runways/Runways.py
@@ -7,18 +7,18 @@ Created on Mon Aug 12 16:29:19 2019
 
 from Database import*
 
-txt = ExtractText(input('The path of the complete OAG document : ')) #'C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/OAG.json'
+txt = ExtractText(input('The path of the complete OAG document : ')) #The document 'OAG.json' inculing the complete OAG Database.
 Data = txt.database_Global()
 print('Number of Years in the Database : ' + str(Data.size()))
 n = 0
 for hmap in Data.htab.values() :
     n = n + hmap.size()
 print('Size of the Database : ' + str(n))
-txt = ExtractText(input('The path of the document containing the Country Codes : ')) #'C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Country_Codes.txt')
+txt = ExtractText(input('The path of the document containing the Country Codes : ')) #'CAEM/Data/Country_Codes.txt'
 Code = txt.countryCode()
-txt = ExtractText(input('The path of the document containing the number of Runways by Airport : ')) #'C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Runways.txt')
+txt = ExtractText(input('The path of the document containing the number of Runways by Airport : ')) #'CAEM/Data/Runways/Runways.txt'
 Runways = txt.runways()
-txt = ExtractText(input('The path of the document containing the layout of Runways by Airport : ')) #'C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Rwys.json')
+txt = ExtractText(input('The path of the document containing the layout of Runways by Airport : ')) #'CAEM/Data/Runways/Rwys.json'
 Additional = ExtractText('C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Rwys_Additional.json')
 Direction = txt.rwys(Additional)
 Runways.fusion(Direction)
