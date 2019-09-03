aa8727a59870026396008168801a41ed78c75b73
Nor97 <53235829+Nor97@users.noreply.github.com>
2019-09-03 15:21:34 -0400

    Update Complete_Classification.py

--- Complete_Classification.py
+++ Complete_Classification.py
@@ -9,18 +9,18 @@ import xlwt
 from Database import*
 
 #Import of the needed documents under a usable format :
-txt = ExtractText('C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/OAG.json') #input('The path of the complete OAG document : ')
+txt = ExtractText(input('The path of the complete OAG document : ')) #'CAEM/Data/OAG Database/OAG.json'
 Data = txt.database_Global()
 print('Number of Years in the Database : ' + str(Data.size()))
 n = 0
 for hmap in Data.htab.values() :
     n = n + hmap.size()
 print('Size of the Database : ' + str(n))
-txt = ExtractText('C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Country_Codes.txt') #input('The path of the document containing the Country Codes : ')
+txt = ExtractText(input('The path of the document containing the Country Codes : ')) #'CAEM/Data/Country_Codes.txt'
 Code = txt.countryCode()
-txt = ExtractText('C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/Runways.json') #input('The path of the document containing the number of Runways by Airport : ')
+txt = ExtractText(input('The path of the document containing the Runway information by Airport : ')) #'CAEM/Data/Runways/Runways.json'
 Runways = txt.rwys_database()
-txt = ExtractText('C:/Users/Maël Akouz/Desktop/OACI/CAEM/Bases_de_Donnees/World_Bank_Income_Classification.txt') #input('The path of the document containing the World Bank Category of Countries : ')
+txt = ExtractText(input('The path of the document containing the World Bank Category of Countries : ')) #'CAEM/Data/World_Bank_Income_Classification.txt'
 Income = txt.income()
 
 #Creation of the needed databases :
